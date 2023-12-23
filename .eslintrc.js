27ad88dcc861ba2934c75eddcdd2d75eb300e694
jobscale <jobscale@users.noreply.github.com>
2023-12-23 16:44:43 +0900

    copied popup added

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,11 @@ module.exports = {
     'class-methods-use-this': 'off',
     'no-await-in-loop': 'off',
     'no-param-reassign': 'off',
+    'object-curly-newline': ['error', {
+      ObjectExpression: { minProperties: 6, multiline: true, consistent: true },
+      ObjectPattern: { minProperties: 6, multiline: true, consistent: true },
+      ImportDeclaration: { minProperties: 6, multiline: true, consistent: true },
+      ExportDeclaration: { minProperties: 6, multiline: true, consistent: true },
+    }],
   },
 };
