0798beac6ec058a9c97120a5db9b9118f713602a
jobscale <jobscale@users.noreply.github.com>
2025-09-09 06:35:27 +0900

    github/workflow updated

---
+++ app/parse-body.js
@@ -0,0 +1,23 @@
+export const parseBody = async req => {
+  const buffer = await new Promise((resolve, reject) => {
+    const chunks = [];
+    req.on('data', chunk => chunks.push(chunk));
+    req.on('end', () => resolve(Buffer.concat(chunks)));
+    req.on('error', e => reject(e));
+  });
+
+  const contentType = req.headers['content-type'] || '';
+  if (contentType.startsWith('application/json')) {
+    req.body = JSON.parse(buffer.toString());
+    return;
+  }
+  if (contentType.startsWith('application/x-www-form-urlencoded')) {
+    const params = new URLSearchParams(buffer.toString());
+    req.body = {};
+    params.entries().forEach(([key, value]) => { req.body[key] = value; });
+  }
+};
+
+export default {
+  parseBody,
+};
