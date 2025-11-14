7f5306afab2cccf9efaa74ab3a9676c637fea4a2
jobscale <jobscale@users.noreply.github.com>
2025-11-14 12:20:06 +0900

    clock design updated

--- docs/el-to-png/index.js
+++ docs/el-to-png/index.js
@@ -26,45 +26,17 @@ const Ocean = {
     { name: '明朝体', class: 'font-mincho' },
     { name: '手書き風', class: 'font-hand' },
   ],
-  fontSize: 'font-size-25',
-  sizeList: [
-    { name: 'size-18', class: 'font-size-18' },
-    { name: 'size-19', class: 'font-size-19' },
-    { name: 'size-20', class: 'font-size-20' },
-    { name: 'size-21', class: 'font-size-21' },
-    { name: 'size-22', class: 'font-size-22' },
-    { name: 'size-23', class: 'font-size-23' },
-    { name: 'size-24', class: 'font-size-24' },
-    { name: 'size-25', class: 'font-size-25' },
-    { name: 'size-26', class: 'font-size-26' },
-    { name: 'size-27', class: 'font-size-27' },
-    { name: 'size-28', class: 'font-size-28' },
-    { name: 'size-29', class: 'font-size-29' },
-    { name: 'size-30', class: 'font-size-30' },
-    { name: 'size-31', class: 'font-size-31' },
-    { name: 'size-32', class: 'font-size-32' },
-    { name: 'size-33', class: 'font-size-33' },
-    { name: 'size-34', class: 'font-size-34' },
-    { name: 'size-35', class: 'font-size-35' },
-    { name: 'size-36', class: 'font-size-36' },
-    { name: 'size-37', class: 'font-size-37' },
-    { name: 'size-38', class: 'font-size-38' },
-    { name: 'size-39', class: 'font-size-39' },
-    { name: 'size-40', class: 'font-size-40' },
-    { name: 'size-41', class: 'font-size-41' },
-    { name: 'size-42', class: 'font-size-42' },
-    { name: 'size-43', class: 'font-size-43' },
-    { name: 'size-44', class: 'font-size-44' },
-  ],
+  fontSize: 'font-size: 25pt;',
+  sizeList: [],
 
   async saveAsPNG(el) {
-    const fname = el.dataset.name;
+    const { name } = el.dataset;
     const canvas = await html2canvas(el, {
       backgroundColor: null,
     });
     const link = document.createElement('a');
     link.href = canvas.toDataURL('image/png');
-    link.download = `${fname.replace(':', '_')}.png`;
+    link.download = `${name.replace(':', '_')}.png`;
     link.click();
     await new Promise(resolve => { setTimeout(resolve, 200); });
     link.remove();
@@ -80,6 +52,11 @@ const Ocean = {
 
   async start() {
     logger.info('start');
+    for (let i = 10; i < 55; i++) {
+      self.sizeList.push({
+        name: `font-size-${i}`, style: `font-size: ${i}pt;`,
+      });
+    }
   },
 };
 
