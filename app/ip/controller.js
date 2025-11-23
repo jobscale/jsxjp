49d40a54f7a1dfc1b9ba05519748a05322618bf1
jobscale <jobscale@users.noreply.github.com>
2025-11-23 20:11:54 +0900

    req.socket? squid

--- app/ip/controller.js
+++ app/ip/controller.js
@@ -1,7 +1,7 @@
 export class Controller {
   ip(req, res) {
     const headers = new Headers(req.headers);
-    const ip = req.socket.remoteAddress || req.ip;
+    const ip = req.socket?.remoteAddress || req.ip;
     const remoteIp = headers.get('X-Real-Ip') || headers.get('X-Forwarded-For') || ip;
     res.end(remoteIp);
   }
