c564eddf164e5266d473d99cc7a5dc5156d670b2
jobscale <jobscale@users.noreply.github.com>
2026-04-01 10:50:17 +0900

    fly.io/fly.toml added

---
+++ fly.io/deploy.sh
@@ -0,0 +1,10 @@
+fly apps create jsxjp
+fly deploy
+fly scale count 1
+fly apps list
+fly status -a jsxjp
+fly certs add jsx.jp
+fly certs add jsx.jp -a jsxjp
+fly certs list -a jsxjp
+fly certs show jsx.jp -a jsxjp
+fly certs check jsx.jp -a jsxjp
