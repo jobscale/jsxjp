96d5ac6ce417d33df008a94344a856d2b27b90c4
jobscale <jobscale@users.noreply.github.com>
2026-04-03 14:05:17 +0900

    fly.io updated

--- fly.io/deploy.sh
+++ fly.io/deploy.sh
@@ -1,9 +1,11 @@
 fly apps create jsxjp
 fly deploy
+fly scale show
 fly scale count 1
+fly machines list
+fly machines destroy e784102dbe3508 --force
 fly apps list
 fly status -a jsxjp
-fly certs add jsx.jp
 fly certs add jsx.jp -a jsxjp
 fly certs list -a jsxjp
 fly certs show jsx.jp -a jsxjp
