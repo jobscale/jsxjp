03873da6da53a7edf26f4c682414970848aa0566
jobscale <jobscale@users.noreply.github.com>
2026-04-08 19:06:20 +0900

    ssh client updated

--- scripts/ssh-test.sh
+++ scripts/ssh-test.sh
@@ -32,10 +32,8 @@ nc -vz -w 1 127.0.0.1 3000 || {
 }
 
 # test ssh connection
-ssh-keygen -f '/home/jobscale/.ssh/known_hosts' -R '[127.0.0.1]:3000' >/dev/null 2>&1
-ssh -o "ProxyCommand ./client.js ws %h %p x.a.jsx.jp 22" 127.0.0.1 -p 3000 echo "hello 001"
-ssh -o "ProxyCommand ./client.js ws %h %p y.a.jsx.jp 22" 127.0.0.1 -p 3000 echo "hello 002"
-ssh -o "ProxyCommand ./client.js ws %h %p z.a.jsx.jp 22" 127.0.0.1 -p 3000 echo "hello 003"
-ssh-keygen -f '/home/jobscale/.ssh/known_hosts' -R '[127.0.0.1]:3000' >/dev/null 2>&1
-ssh -o "ProxyCommand ./client.js ws %h %p us.jsx.jp 2022" 127.0.0.1 -p 3000 echo "hello 004"
+ssh -o "ProxyCommand ./client.js ws://127.0.0.1:3000  %h %p" x.a.jsx.jp -p 22 echo "hello 001"
+ssh -o "ProxyCommand ./client.js ws://127.0.0.1:3000  %h %p" y.a.jsx.jp -p 22 echo "hello 002"
+ssh -o "ProxyCommand ./client.js ws://127.0.0.1:3000  %h %p" z.a.jsx.jp -p 22 echo "hello 003"
+ssh -o "ProxyCommand ./client.js ws://127.0.0.1:3000  %h %p" us.jsx.jp -p 22 echo "hello 004"
 echo "SSH connection test passed"
