f54be29a6596375937e7c59e03eaf28a47d9fc41
jobscale <jobscale@users.noreply.github.com>
2026-04-08 12:58:11 +0900

    ws added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import http from 'http';
 import './app/config/index.js';
 import { logger } from '@jobscale/logger';
-import { app, errorHandler } from './app/index.js';
+import { app, upgradeHandler, errorHandler } from './app/index.js';
 
 const PORT = Number.parseInt(process.env.PORT || 3000, 10);
 
 const main = async () => {
   const server = http.createServer(app);
+  server.on('upgrade', upgradeHandler);
   server.on('error', errorHandler);
   const options = {
     host: '0.0.0.0',
