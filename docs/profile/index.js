ef5fe5329d1f0f93e2730d8e3145c07b1ca90cba
jobscale <jobscale@users.noreply.github.com>
2026-06-04 11:04:41 +0900

    change theme style updated

--- docs/profile/index.js
+++ docs/profile/index.js
@@ -4,20 +4,11 @@ import { logger } from 'https://esm.sh/@jobscale/logger';
 let self = {
   onColorScheme() {
     const html = document.documentElement;
-    if (html.style.colorScheme === 'dark') {
-      html.style.colorScheme = 'light';
-      html.classList.toggle('light', true);
-      html.classList.toggle('dark', false);
-    } else if (html.style.colorScheme === 'light') {
-      html.style.colorScheme = 'dark';
-      html.classList.toggle('light', false);
-      html.classList.toggle('dark', true);
-    } else {
-      const isDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-      html.style.colorScheme = isDark ? 'dark' : 'light';
-      html.classList.toggle('light', !isDark);
-      html.classList.toggle('dark', isDark);
-    }
+    const current = html.style.colorScheme;
+    const next = current ? current === 'dark' ? 'light' : 'dark'
+    : window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    html.style.colorScheme = next;
+    html.dataset.theme = next;
   },
 };
 self = reactive(self);
