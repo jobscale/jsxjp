4621739524a509790037fc9ae71c59d06b1f7936
jobscale <jobscale@users.noreply.github.com>
2026-08-18 09:57:26 +0900

    cdk api update

--- cdk-serverless/lib/functions/user/welcome/index.js
+++ cdk-serverless/lib/functions/user/welcome/index.js
@@ -1,3 +1,6 @@
+import { validation } from '../../../../../app/auth/validation.js';
+// import { service as auth } from '../../../../../app/auth/service.js';
+
 const { ENV } = process.env;
 
 const logger = new Proxy(console, {
@@ -8,13 +11,19 @@ const logger = new Proxy(console, {
 
 export const handler = async event => {
   logger.info('EVENT:', JSON.stringify(event, null, 2));
+
+  await validation.login({ body: JSON.parse(event.body) });
+  // await auth.login(event);
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json; charset=utf-8' },
     body: `${JSON.stringify({
       message: 'Welcome',
       env: ENV || 'undefined',
-      input: event,
+      'input event': event,
     }, null, 2)}\n`,
   };
 };
+
+// example
+// curl -i -X POST https://dev-serverless.jsx.jp/user/welcome --data '{"login":"guest","password":"secret"}' -H 'Content-Type: application/json'
