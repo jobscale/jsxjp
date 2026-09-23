26886a34662dc7859ca0b5644fa461040f2a12a5
jobscale <jobscale@users.noreply.github.com>
2026-09-24 06:40:25 +0900

    api endpoint updated

--- cdk-app/lib/functions/proxy/docs/v1/js/fetch-api.js
+++ cdk-app/lib/functions/proxy/docs/v1/js/fetch-api.js
@@ -1,5 +1,5 @@
 export const fetchApi = (path, opts) => {
-  const endpointApi = 'https//api.jsx.jp';
+  const endpointApi = 'https://api.jsx.jp';
   const pathApi = `${endpointApi}${path}`;
   return fetch(pathApi, opts);
 };
